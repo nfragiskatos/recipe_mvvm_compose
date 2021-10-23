fcb5cca7397fe050a861b751722df199cab0ed26
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-10-23 09:59:10 -0700

    Initial commit

---
+++ app/src/main/java/com/nfragiskatos/recipe_mvvm_compose/ui/theme/Color.kt
@@ -0,0 +1,29 @@
+package com.nfragiskatos.recipe_mvvm_compose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+// ----- primary
+// Main
+val Cyan300 = Color(0xFF4FDEE9)
+
+// Light
+val Cyan200 = Color(0xFF8BFFFF)
+
+// Dark
+val Cyan600 = Color(0xFF00ACB7)
+
+
+// ----- Secondary
+// Main
+val Red400 = Color(0xFFE9594F)
+
+// Light
+val Red300 = Color(0xFFFF8B7B)
+
+// Dark
+val Red900 = Color(0xFFB12426)
