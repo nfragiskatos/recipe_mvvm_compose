7589cf1c53f8a1271e66b8c676d1249981ec5f38
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-10-24 06:06:06 -0700

    - Added dependencies for Hilt (for later) - Added dependencies for MockWebServer - Updated RecipeAPIServiceTest.kt to use a mock web server instead. - updated unit test to make sure the body wasn't null, and  that the request path + header were correctly configured - added unit test to ensure the correct result page size

--- app/src/test/java/com/nfragiskatos/recipe_mvvm_compose/api/RecipeAPIServiceTest.kt
+++ app/src/test/java/com/nfragiskatos/recipe_mvvm_compose/api/RecipeAPIServiceTest.kt
@@ -7,39 +7,59 @@ import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import com.google.common.truth.Truth.assertThat
+import com.nfragiskatos.recipe_mvvm_compose.BuildConfig
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okio.buffer
+import okio.source
 
-class RecipeAPIServiceTest {
+class RecipeAPIServiceTest() {
 
     private lateinit var service: RecipeAPIService
+    private lateinit var server: MockWebServer
 
     @Before
     fun setUp() {
-        val retrofit = Retrofit.Builder()
+        server = MockWebServer()
+        service = Retrofit.Builder().baseUrl(server.url(""))
             .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl("https://food2fork.ca")
             .build()
-        service = retrofit.create(RecipeAPIService::class.java)
+            .create(RecipeAPIService::class.java)
+    }
+
+    private fun enqueueMockResponse(fileName: String) {
+        val inputStream = javaClass.classLoader!!.getResourceAsStream(fileName)
+        val source = inputStream.source().buffer()
+        val mockResponse = MockResponse()
+        mockResponse.setBody(source.readString(Charsets.UTF_8))
+        server.enqueue(mockResponse)
     }
 
     @Test
     fun getSearchedRecipes_sentRequest_receivedExpected() {
         runBlocking {
-            val response = service.getSearchedRecipes(
+
+            enqueueMockResponse("reciperesponse.json")
+
+            val body = service.getSearchedRecipes(
                 1,
                 "chicken"
-            )
+            ).body()
+            val request = server.takeRequest()
+            val authHeader = request.getHeader("Authorization")
 
-            assertThat(response.isSuccessful).isTrue()
-
-            val body = response.body()
 
             assertThat(body).isNotNull()
+            assertThat(request.path).isEqualTo("/api/recipe/search?page=1&query=chicken")
+            assertThat(authHeader).isEqualTo("Token 9c8b06d329136da358c2d00e76946b0111ce2c48")
         }
     }
 
     @Test
     fun getSearchedRecipes_receivedResponse_correctPageSize() {
         runBlocking {
+            enqueueMockResponse("reciperesponse.json")
+
             val body = service.getSearchedRecipes(
                 1,
                 "chicken"
