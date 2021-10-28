97bf26e0beda374e43e62eba70b3b62913ff07f4
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-10-27 20:49:41 -0700

    - centered the loading spinner

--- app/src/main/java/com/nfragiskatos/recipe_mvvm_compose/ui/components/CircularIndeterminateProgressBar.kt
+++ app/src/main/java/com/nfragiskatos/recipe_mvvm_compose/ui/components/CircularIndeterminateProgressBar.kt
@@ -1,9 +1,13 @@
 package com.nfragiskatos.recipe_mvvm_compose.ui.components
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -15,14 +19,14 @@ fun CircularIndeterminateProgressBar(
     if (isDisplayed) {
         Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
+                .fillMaxSize()
                 .padding(50.dp),
             horizontalArrangement = Arrangement.Center
         ) {
 
             CircularProgressIndicator(
-                color = MaterialTheme.colors.primary
+                color = MaterialTheme.colors.primary,
+                modifier = Modifier.align(Alignment.CenterVertically)
             )
         }
     }
